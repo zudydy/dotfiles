808c922e5142bfbdb53589bfc2559023714b5713
zudydy <kjyjykwak@gmail.com>
2025-12-23 11:27:58 +0900

    feat(zsh): remove nvm config

--- zsh/10-path.zsh
+++ zsh/10-path.zsh
@@ -8,13 +8,10 @@ path=(
   /usr/sbin
   /sbin
   /Applications/WebStorm.app/Contents/MacOS
-  /Users/jaeyeong/.codeium/windsurf/biㅂn
+  /Users/jaeyeong/.codeium/windsurf/bin
   /Users/jaeyeong/Library/pnpm
   $path  # 기존 path 유지
 )
 
 export PATH
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
